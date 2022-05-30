fedcd6e6d022af70342be2d0039ca3002d13cf8b
Birzhan Serik <birzhans14@gmail.com>
2022-05-31 01:32:40 +0600

    message template

--- app/helpers/chats_helper.rb
+++ app/helpers/chats_helper.rb
@@ -2,4 +2,8 @@ module ChatsHelper
   def chat_preview_class(current_chat, chat)
     (current_chat == chat) ? 'text-primary' : 'text-dark'
   end
+
+  def message_class(message, current_user)
+    "rounded p-2 col #{(message.user == current_user) ? 'my-message' : 'other-message'}"
+  end
 end
